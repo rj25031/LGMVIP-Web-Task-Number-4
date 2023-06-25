58f22a33a5d185f25d8ed562f88c1635be7a99f4
Rupesh <rj25031@gmail.com>
2023-06-26 00:12:29 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const keys = document.getElementsByClassName("keys")[0];
+const display = document.getElementsByClassName("display")[0];
+var str='';
+keys.addEventListener("click", (e)=>{
+    const target = e.target;
+    const value = target.innerHTML
+    if(target.classList.contains('all-clear')){
+        allClear();
+    }else if(target.classList.contains('key')){
+        str=str+value;
+        display.innerHTML=str;
+    }else if(target.classList.contains('delete')){
+        backspace();
+    }else if(target.classList.contains('plus-minus')){
+        plusMinus();
+    }else if(target.classList.contains('equal-to')){
+        result();
+    }
+})
+
+document.addEventListener("keydown", (e)=>{
+    if(e.key>=0 && e.key<=9 ||e .location === KeyboardEvent.DOM_KEY_LOCATION_NUMPAD && e.key!="Enter"){
+        console.log(e.key);
+        console.log(typeof(e.key));
+        str=str+e.key;
+        display.innerHTML=str;
+    }else if(e.key==="Enter"||e.key==="Enter" && e.location === KeyboardEvent.DOM_KEY_LOCATION_NUMPAD){
+        result();
+    }else if(e.key==="Backspace"){
+        backspace();
+    }
+})
+
+function allClear(){
+    str='';;
+    display.innerHTML=str;
+}
+
+function backspace(){
+    str=str.substring(0,str.length-1);
+    display.innerHTML=str;
+}
+
+function result(){
+    str=eval(str);
+    str=str.toString();
+    display.innerHTML=str;
+}
+
+function plusMinus(){
+    let val = Number.parseInt(str)
+    str=str+togglePlusMinus(val).toString()
+    display.innerHTML=str;
+}
+
+  
